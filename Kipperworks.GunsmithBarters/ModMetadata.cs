71a65884117afbb20aeda262307bf6722820f56e
Daniel Møgelgaard Pedersen <dmpe@kipperworks.dk>
2026-07-25 15:54:15 +0200

    Fix version verification

--- Kipperworks.GunsmithBarters/ModMetadata.cs
+++ Kipperworks.GunsmithBarters/ModMetadata.cs
@@ -1,4 +1,3 @@
-using System.Reflection;
 using SPTarkov.Server.Core.Models.Spt.Mod;
 
 namespace Kipperworks.GunsmithBarters;
@@ -9,21 +8,12 @@ public record ModMetadata : AbstractModMetadata
     public override string Name { get; init; } = "GunsmithBarters";
     public override string Author { get; init; } = "Solethia";
     public override List<string>? Contributors { get; init; }
-    public override SemanticVersioning.Version Version { get; init; } = GetAssemblyModVersion();
+    public override SemanticVersioning.Version Version { get; init; } = new(ModInfo.Version);
     public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.0");
     public override List<string>? Incompatibilities { get; init; }
     public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
     public override string? Url { get; init; } = "https://forge.sp-tarkov.com/mod/1963/gunsmith-barters";
     public override bool? IsBundleMod { get; init; } = false;
     public override string License { get; init; } = "MIT";
-
-    private static SemanticVersioning.Version GetAssemblyModVersion()
-    {
-        var informationalVersionString = typeof(ModMetadata).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
-        if (informationalVersionString is null)
-            throw new InvalidOperationException("GunsmithBarters Assembly version not found");
-
-        var informationalVersion = new SemanticVersioning.Version(informationalVersionString);
-        return new(informationalVersion.Major, informationalVersion.Minor, informationalVersion.Patch);
-    }
+    
 }
